5053e3b7e68f9098518b9d5d0b2497fcc421f04c
Richard Dyson <richard36@gmail.com>
2018-03-30 16:44:25 +0100

    Bug fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,9 +9,6 @@ chown -R uo:uo /UO
 cd /UO/ServUO
 sed -i -- 's/CompilerResults results = provider.CompileAssemblyFromFile( parms, "" );/CompilerResults results = provider.CompileAssemblyFromFile( parms, files );/g' ./Server/ScriptCompiler.cs
 echo "CustomPath=/client" >> /UO/ServUO/Config/DataPath.cfg
-if [ ! -f /UO/ServUO/Scripts/Misc/AccountPrompt.cs ]; then
-    cp /AccountPrompt.cs /UO/ServUO/Scripts/Misc/
-fi
-rm /AccountPrompt.cs
+mv /AccountPrompt.cs /UO/ServUO/Scripts/Misc/
 make clean && make build
 exec gosu uo /UO/ServUO/ServUO.sh
